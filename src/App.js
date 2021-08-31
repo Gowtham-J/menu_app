432deacb0e024ab6f5cb3aa53b3f0d20f03e2032
gowtham <gowthamj120@gmail.com>
2021-08-31 19:10:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
+  const category = ["all", ...new Set(items.map((item) => item.category))];
+
+  const [menuItem, setMenuItem] = useState(items);
+  const [categories, setCategories] = useState(category);
+
+  const filterItem = (category) => {
+    if (category === "all") {
+      return setMenuItem(items);
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItem(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2 className="title">our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItem={filterItem} />
+        <Menu items={menuItem} />
+      </section>
+    </main>
   );
 }
 
---
+++ src/Categories.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Categories = ({ categories, filterItem }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            key={index}
+            className="filter-btn"
+            onClick={() => filterItem(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
